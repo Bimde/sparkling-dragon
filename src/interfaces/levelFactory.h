1cbb95d2713c94e327b9b7b2f47ff193c44d239e
maxgao18 <maxgao18@yahoo.com>
2019-07-01 22:42:02 -0400

    shaq2

--- src/interfaces/levelFactory.h
+++ src/interfaces/levelFactory.h
@@ -5,6 +5,12 @@
 
 #include "levelInterface.h"
 
+class LevelZero;
+class LevelOne;
+class LevelTwo;
+class LevelThree;
+class LevelFour;
+
 struct LevelConfig {
 	const std::string filename;
 
@@ -13,11 +19,11 @@ struct LevelConfig {
 };
 
 class LevelFactory {
-	std::shared_ptr<LevelInterface> levelZero;
-	std::shared_ptr<LevelInterface> levelOne;
-	std::shared_ptr<LevelInterface> levelTwo;
-	std::shared_ptr<LevelInterface> levelThree;
-	std::shared_ptr<LevelInterface> levelFour;
+	std::shared_ptr<LevelZero> levelZero;
+	std::shared_ptr<LevelOne> levelOne;
+	std::shared_ptr<LevelTwo> levelTwo;
+	std::shared_ptr<LevelThree> levelThree;
+	std::shared_ptr<LevelFour> levelFour;
 
   public:
   	LevelFactory(LevelConfig cfg);
